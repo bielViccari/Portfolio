2a6430363a1e04dbb82a3ef28e5d2353d5b42b40
Gabriel Viccari <gabriel.viccari20@gmail.com>
2024-04-02 00:39:27 -0300

    target blank to links.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,20 +9,19 @@ export const Footer = () => {
                         <p>
                             Entre em contato para possíveis freelances, ou vagas de emprego!
                         </p>
-                        <a href="https://api.whatsapp.com/send/?phone=18981469767" className="see__more">entrar em contato</a>
+                        <a target='_blank' href="https://api.whatsapp.com/send/?phone=18981469767" className="see__more">entrar em contato</a>
                     </div>
                     <div className="content">
                         <img src={logo} alt="logo" />
                         <p>Aprender, Desenvolver e solucionar.</p>
                         <div className="links">
-                            <a href="#" target="_blank" className="see__more"><i className="fab fa-instagram"></i></a>
                             <a href="https://github.com/bielViccari" className="see__more"><i className="fab fa-github"></i></a>
                             <a href="https://www.linkedin.com/in/gabriel-viccari-de-almeida/" target="_blank" className="see__more"><i className="fab fa-linkedin"></i></a>
                             <a href="mailto:gabriel.viccari20@gmail.com" target="_blank" className="see__more"><i className="fa fa-envelope"></i></a>
                         </div>
-                        <a href="#header" class="back__to__top">
-                            <div class="container2">
-                                <i class="fa fa-arrow-up arrow"></i>
+                        <a href="#header" className="back__to__top">
+                            <div className="container2">
+                                <i className="fa fa-arrow-up arrow"></i>
                                 <span className='backToTop'>Voltar ao Topo</span>
                             </div>
                         </a>
--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -22,7 +22,7 @@ export const Works = () => {
                                      que publicava os produtos, e os produtos possuiam link de afiliado, para o usuário ganhar comissao por venda da 
                                     loja oficial.<br />(projeto descontinuado)
                                 </p>
-                                <a href="https://github.com/bielViccari/achadosZZZ">Veja o código do projeto</a>
+                                <a target='_blank' href="https://github.com/bielViccari/achadosZZZ">Veja o código do projeto</a>
                             </div>
                         </div>
                         <div className="project">
@@ -33,7 +33,7 @@ export const Works = () => {
                                     O site tem o propósito de fazer com que os clientes publiquem roupas para doação, e os outros comentem na 
                                     publicação, asssim como uma rede social de doações.
                                 </p>
-                                <a href="https://github.com/bielViccari/tcc">Veja o código do projeto</a>
+                                <a target='_blank' href="https://github.com/bielViccari/tcc">Veja o código do projeto</a>
                             </div>
                         </div>
                         <div className="project">
@@ -44,7 +44,7 @@ export const Works = () => {
                                     ter o controle o salário que está sendo gasto com cada um, para facilitar o controle de funcionários e de 
                                     dinheiro gasto pela empresa.<br /> (Projeto em Desenvolvimento).
                                 </p>
-                                <a href="https://github.com/bielViccari/super_gestao">Veja o código do projeto</a>
+                                <a target='_blank' href="https://github.com/bielViccari/super_gestao">Veja o código do projeto</a>
                             </div>
                         </div>
                     </div>
